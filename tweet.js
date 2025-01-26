c932cc1ab0d6d258ff368c81b0d5613445f66123
meme2wsx <meme2wsx@gmail.com>
2025-01-27 08:36:50 +0900

    a

--- tweet.js
+++ tweet.js
@@ -11,7 +11,7 @@ const client = new TwitterApi({
 // 素敵な文字列
 const fortunes = [
   "どんな馬鹿でも、あら探しをしたり、難癖をつけたり、苦情を言ったりできるワオ。",
-  "精神的に向上心のない者は馬鹿だワオ。。",
+  "精神的に向上心のない者は馬鹿だワオ。",
   "愛国者──政治家には馬鹿みたいにだまされ、征服者には手もなく利用される人間ワオ。",
   "馬鹿には会いたくないというのなら、まず自分の鏡を壊すことだワオね。",
   "馬鹿と議論するな。はた目には、どちらが馬鹿か分からないワオよ。",
@@ -26,7 +26,7 @@ const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)];
 (async () => {
   try {
     // 選んだ文字列をツイートするワオ。
-    const tweet = await client.v2.tweet(`今日のお言葉: ${randomFortune}`);
+    const tweet = await client.v2.tweet(randomFortune);
     console.log('ツイート成功:', tweet);
   } catch (error) {
     console.error('エラー:', error);
